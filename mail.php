584f228cb5c8c309e67d33ac6c200b7846cd4310
Gentlemean82 <122789476+Gentlemean82@users.noreply.github.com>
2023-01-16 17:43:43 +0500

    updated

--- mail.php
+++ mail.php
@@ -4,10 +4,10 @@ $name = $_POST['Name'];
 $email= $_POST['Email'];
 $phone= $_POST['Phone'];
 $text= $_POST['text']
-$to = "hassaanulhaq822gmail.com";
+$to = "hassaanulhaq82@gmail.com";
 $subject = "Mail From SLS";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone =" .$phone . "\r\n text =$text";
-$headers = "From: http://127.0.0.1:5500/Bujinech.html" . "\r\n" .
+$headers = "From: index.html" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
